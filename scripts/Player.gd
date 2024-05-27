b0d4bb520f503e65c3bd7d5f37ea4b281912036f
Wayne Lee <waynel1996weiyang@gmail.com>
2024-05-27 14:26:52 -0700

    Implement stop control and remapped jump and shoot

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -15,19 +15,22 @@ func _physics_process(delta : float):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	if Input.is_action_just_pressed("Up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if not Input.is_action_pressed("Stop"):
+		if Input.is_action_just_pressed("Jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	var direction = Input.get_axis("Left", "Right")
-	if direction:
-		velocity.x = direction * SPEED
+		var direction = Input.get_axis("Left", "Right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
 	
 func _process(delta : float):
-	if Input.is_anything_pressed():
+	if Input.is_action_pressed("Left") or Input.is_action_pressed("Right") or Input.is_action_pressed("Up") or Input.is_action_pressed("Down"):
 		var vector = Input.get_vector("Left", "Right", "Up", "Down")
 		gun.set_rotation(vector.angle())
 	
