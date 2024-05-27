75195a12c349fde9b47aac50dbc35b68c2d221fa
Wayne Lee <waynel1996weiyang@gmail.com>
2024-05-26 18:36:32 -0700

    Add bullet

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,22 +1,22 @@
 extends CharacterBody2D
 
+@onready var gun = %Gun
+@onready var gunDelay = %GunDelay
+
+var BULLET = preload("res://scene/bullet.tscn")
+
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _physics_process(delta):
-	# Add the gravity.
+func _physics_process(delta : float):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle Jump.
 	if Input.is_action_just_pressed("Up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -24,3 +24,13 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+func _process(delta : float):
+	if Input.is_action_pressed("Shoot") and gunDelay.is_stopped():
+		_shoot()
+		gunDelay.start()
+		
+func _shoot():
+	var bullet = BULLET.instantiate()
+	bullet.transform = gun.global_transform
+	get_parent().add_child(bullet)
