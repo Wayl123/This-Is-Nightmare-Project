314a089ee1ba6803c1e54e17b412cfb4b070a3f6
Wayne Lee <waynel1996weiyang@gmail.com>
2024-06-02 23:17:32 -0700

    Add player to title screen

--- scripts/Gun.gd
+++ scripts/Gun.gd
@@ -3,8 +3,6 @@ extends Node2D
 @onready var gunMuzzle = %GunMuzzle
 @onready var gunDelay = %GunDelayTimer
 
-@onready var bossStage = get_tree().get_first_node_in_group("BossStage")
-
 var BULLET = preload("res://scene/bullet.tscn")
 
 func shoot():
@@ -13,4 +11,4 @@ func shoot():
 	var bullet = BULLET.instantiate()
 	bullet.add_to_group("PlayerBullets")
 	bullet.transform = gunMuzzle.global_transform
-	bossStage.add_child(bullet)
+	get_node("/root/SceneSwitcher/.").add_child(bullet)
